4667e4e0746fc262a3c34035c801d9f71973d2d6
Martin Danev <martin.danev01@gmail.com>
2019-03-04 21:12:29 +0100

    Enemy Shooting System

--- LaserDefender/Assets/Scripts/enemy.cs
+++ LaserDefender/Assets/Scripts/enemy.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class enemy : MonoBehaviour {
 
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float mintimeShots = 0.2f;
+    [SerializeField] float maxTimeShots = 3f;
+    [SerializeField] GameObject enemyLaser;
+    [SerializeField] float enemyLaserSpeed = 100f;
+
+
+    Coroutine enemyFire;
 
 
     void Start() {
-        
+        shotCounter = Random.Range(mintimeShots, maxTimeShots);
     }
 
     // Update is called once per frame
     void Update() {
-        
+        countdownShoot();
+    }
+
+    private void countdownShoot() {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f) {
+            Fire();
+            shotCounter = Random.Range(mintimeShots, maxTimeShots);
+        } 
+    }
+
+    private void Fire() {
+        GameObject laser = Instantiate(enemyLaser, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
